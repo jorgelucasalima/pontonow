a514b4bc6b7ce2cbf4b697904674c8c7a8cf8996
jorgelucasalima <jorgelucasalima@gmail.com>
2022-06-30 11:24:49 -0300

    correção de bug ao cadastrar pontos e implementações

--- src/components/ModalBatidaPonto/index.jsx
+++ src/components/ModalBatidaPonto/index.jsx
@@ -16,29 +16,36 @@ export function ModalBatidaPonto(props) {
     const mes = instaciaData.getMonth() + 1
     const ano = instaciaData.getFullYear()
 
-    const idDataString = dia+'-'+mes+'-'+ano
-
-  
+    const idDataString = (dia+'-'+mes+'-'+ano).toString()
+   
     const db = firebase.firestore();
     const DocumentRef = db.collection('ponto').doc(idDataString.toString());
-    console.log(DocumentRef.id)
-
+    
     const doc = await DocumentRef.get()
-    console.log(doc.data())
+    
 
-    if (DocumentRef.id !== doc.data().uid_documento ) {
-      await DocumentRef.set({
-        uid_documento: DocumentRef.id,
-        uid_usuario: props.uid,
-        inicio_expediente: props.dataAtual,
-        inicio_intervalo: '',
-        fim_intervalo: '',
-        fim_expediente: '',
-      })
-      console.log('CHEGANDO NO PRIMEIRO IF')
-    } else  {
-      console.log('CHEGANDO NO SEGUNDO IF')
+    //verificar se existe algum documento direfente da data de hoje 
+    // se meu documento.id for diferente da data de hoje ele adiciona 
+    if (DocumentRef.id !== idDataString ) {
+      try {
+        await DocumentRef.set({
+          uid_documento: DocumentRef.id,
+          uid_usuario: props.uid,
+          inicio_expediente: props.dataAtual,
+          inicio_intervalo: '',
+          fim_intervalo: '',
+          fim_expediente: '',
+        })
+        props.onRequestClose()
+        toast.success('Ponto registrado com sucesso!')
+      } catch (error) {
+          props.onRequestClose()
+          toast.error('Ocorreu um problema ao registrar o ponto!')
+      }
+        
+      }  else  {
         try {
+          //verificar se inicio expediente esta vazio e inicio intervalo ta nulo e adiciona
           if (doc.data().inicio_expediente !== '' && doc.data().inicio_intervalo === '') {
             await DocumentRef.update({
               inicio_intervalo: props.dataAtual,
@@ -46,14 +53,29 @@ export function ModalBatidaPonto(props) {
             props.onRequestClose()
             toast.success('Ponto registrado com sucesso!')
           }
-        
+          //verifica se o inicio intervalo esta vazio e fim intervalo ta nulo e adiciona
+            else if (doc.data().inicio_intervalo !== '' && doc.data().fim_intervalo === '') {
+              await DocumentRef.update({
+                fim_intervalo: props.dataAtual,
+              })
+              props.onRequestClose()
+              toast.success('Ponto registrado com sucesso!')
+            }
+            //verifica se o fim intervalo esta vazio e fim expediente ta nulo e adiciona
+            else if (doc.data().fim_intervalo !== '' && doc.data().fim_expediente === '') {
+              await DocumentRef.update({
+                fim_expediente: props.dataAtual,
+              })
+              props.onRequestClose()
+              toast.success('Ponto registrado com sucesso!')
+            }
         }
         catch (error) {
-          console.log(error)
+          props.onRequestClose()
+          toast.error('Ocorreu um problema ao registrar o ponto!')
         }
     }
-
-}
+  }
 
 
 
